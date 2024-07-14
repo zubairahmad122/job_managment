a3358371658c27286ca75a2e26a56121c4c53e2f
zubairahmad122 <154881585+zubairahmad122@users.noreply.github.com>
2024-07-14 10:12:58 -0700

    Update index.jsx

--- fronted/src/pages/JobFrom/index.jsx
+++ fronted/src/pages/JobFrom/index.jsx
@@ -51,7 +51,7 @@ const index = () => {
 
 
     try {
-      const response = await axios.post(`${url}api/v1/application/apply`, formData);
+      const response = await axios.post(`${url}/api/v1/application/apply`, formData);
       toast.success("We received your application successfully. Our team will contact you within 24 hours.");
       setFormData({
         name: "",
